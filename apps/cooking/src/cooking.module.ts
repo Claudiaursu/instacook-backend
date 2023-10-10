038331d36e52b2eef69d86c6d05496788751c359
claudia <ursu.claudia99@yahoo.com>
2023-10-10 23:35:36 +0300

    docker stuff

--- apps/cooking/src/cooking.module.ts
+++ apps/cooking/src/cooking.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CookingController } from './cooking.controller';
 import { CookingService } from './cooking.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './apps/cooking/.env',
+    }),
+    // RmqModule.register({
+    //   name: BILLING_SERVICE,
+    // }),
+    // AuthModule,
+  ],
   controllers: [CookingController],
   providers: [CookingService],
 })
