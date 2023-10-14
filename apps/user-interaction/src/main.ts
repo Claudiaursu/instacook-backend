e806b95d0bcd6fd4aa0af17fb461e94a04143be6
claudia <ursu.claudia99@yahoo.com>
2023-10-15 00:37:55 +0300

    rabbitmq module

--- apps/user-interaction/src/main.ts
+++ apps/user-interaction/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { UserInteractionModule } from './user-interaction.module';
+import { RabbitmqService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserInteractionModule);
-  await app.listen(3000);
+  const rabbitmqService = app.get<RabbitmqService>(RabbitmqService);
+  app.connectMicroservice(rabbitmqService.getOptions('USER_INTERACTION'));
+  //await app.listen(3000);
+  app.startAllMicroservices();
 }
 bootstrap();
--- apps/user-interaction/src/user-interaction.module.ts
+++ apps/user-interaction/src/user-interaction.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserInteractionController } from './user-interaction.controller';
 import { UserInteractionService } from './user-interaction.service';
+import { RabbitmqModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RabbitmqModule,
+  ],
   controllers: [UserInteractionController],
   providers: [UserInteractionService],
 })
